a085b8f60571f24672d8bfdfd3f845ac239ac821
Omer Gillani <omer.gillani@halliburton.com>
2018-09-14 19:57:21 +0500

    Implement table for bulkdata

--- sql12/core/src/net/sourceforge/squirrel_sql/fw/datasetviewer/CellDataPopup.java
+++ sql12/core/src/net/sourceforge/squirrel_sql/fw/datasetviewer/CellDataPopup.java
@@ -19,12 +19,27 @@ package net.sourceforge.squirrel_sql.fw.datasetviewer;
  */
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.prefs.Preferences;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+
+import com.lgc.bulkdata.BulkData;
+import com.lgc.bulkdata.BulkDataException;
+import com.lgc.bulkdata.BulkDataFactory;
+import com.lgc.bulkdata.BulkDataSerialType;
+import com.lgc.bulkdata.DataTypeDef;
+import com.lgc.bulkdata.NullValueDef;
+import com.lgc.bulkdata.ProtoBufBulkData;
 
 //import net.sourceforge.squirrel_sql.client.gui.AboutBoxDialog;
 import net.sourceforge.squirrel_sql.client.Main;
+import net.sourceforge.squirrel_sql.fw.datasetviewer.cellcomponent.BlobDescriptor;
 import net.sourceforge.squirrel_sql.fw.datasetviewer.cellcomponent.CellComponentFactory;
 import net.sourceforge.squirrel_sql.fw.gui.GUIUtils;
 import net.sourceforge.squirrel_sql.fw.util.StringManager;
@@ -73,9 +88,9 @@ public class CellDataPopup
 
       Component parent = SwingUtilities.windowForComponent(table);
 
-      final TextAreaDialog dialog =
-         new TextAreaDialog(table, table.getColumnName(col), colDef, obj,
+      final JDialog dialog = getDialog(table, table.getColumnName(col), colDef, obj,
             row, col, isModelEditable, table);
+
 
       dialog.pack();
 
@@ -126,6 +141,23 @@ public class CellDataPopup
 		});
 
 		dialog.setVisible(true);
+	}
+
+
+	private JDialog getDialog(JTable table, String columnName, ColumnDisplayDefinition colDef, Object obj, int row,
+			int col, boolean isModelEditable, JTable table2) {
+		
+		JDialog dialog = null;
+		
+		if (!(obj instanceof BlobDescriptor)) {
+	        dialog = new TextAreaDialog(table, table.getColumnName(col), colDef, obj,
+	                row, col, isModelEditable, table);
+		} else {
+	        dialog = new TableDialog(table, table.getColumnName(col), colDef, obj,
+	                row, col, isModelEditable, table);
+		}
+        
+        return dialog;
 	}
 
 
@@ -269,6 +301,153 @@ public class CellDataPopup
          GUIUtils.enableCloseByEscape(this);
 		}
 
+   }
+	
+   class TableDialog extends JDialog {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public TableDialog(Component comp, String columnName, ColumnDisplayDefinition colDef,
+			Object value, int row, int col,
+			boolean isModelEditable, JTable table) {
+		
+		
+		super(SwingUtilities.windowForComponent(comp), s_stringMgr.getString("cellDataPopup.valueofColumn", columnName));
+		
+		try {
+			
+			BlobDescriptor blobDescriptor = (BlobDescriptor) value;
+			BulkData dataObjIn = BulkDataFactory.createWrapper(BulkDataSerialType.PROTOBUF, new NullValueDef(), blobDescriptor.getBlob());
+			
+			String [] columns = ((ProtoBufBulkData)dataObjIn).getColumnNames().toArray(new String[dataObjIn.getColumnCount() + 1]);
+
+			for (int l = columns.length-1; l >= 1; l--) {
+				columns[l] = columns[l-1]; 
+			}
+			
+			columns[0] = "";
+			
+			Object[][] data = new Object[dataObjIn.getRowCount() + 3][dataObjIn.getColumnCount() + 1];
+
+			data[0][0] = "Unit Type";
+			data[1][0] = "Unit";
+			data[2][0] = "Data Type";
+			
+			int k = 1;
+			for(int j = 1; j < columns.length; j++, k++) {
+				
+				data[0][k] = dataObjIn.getColumnUnitTypeName(columns[j]);
+				data[1][k] = dataObjIn.getColumnUnitName(columns[j]);
+				data[2][k] = dataObjIn.getColumnType(columns[j]).name();
+				
+				DataTypeDef type = dataObjIn.getColumnType(columns[j]);
+				Object values = (Object) dataObjIn.getColumnArray(columns[j]);
+				
+				if (DataTypeDef.STRING.equals(type)) {
+					String [] strValues = (String []) values;
+					
+					for (int s = 0; s < strValues.length; s++) {
+						data[s + 3][k] = strValues[s];
+					}
+					
+				} else if (DataTypeDef.FLOAT.equals(type)) {
+					float [] fValues = (float []) values;
+					
+					for (int s = 0; s < fValues.length; s++) {
+						data[s + 3][k] = fValues[s];
+					}
+					
+				} else if (DataTypeDef.DOUBLE.equals(type)) {
+					double [] dValues = (double []) values;
+					
+					for (int s = 0; s < dValues.length; s++) {
+						data[s + 3][k] = dValues[s];
+					}
+					
+				} else if (DataTypeDef.INTEGER.equals(type)) {
+					int [] iValues = (int []) values;
+					
+					for (int s = 0; s < iValues.length; s++) {
+						data[s + 3][k] = iValues[s];
+					}
+				}				
+			}
+			
+			
+			
+/*			Iterator<Iterator<Object>> rows = dataObjIn.getRows();
+			int i = 3;
+			while(rows.hasNext()) {
+				
+				// Setting row number
+				data[i][0] = i - 2;
+				
+				Iterator<Object> rowData = rows.next();
+				int j = 1;
+				while(rowData.hasNext()) {
+					data[i][j++] = rowData.next();
+				}
+				
+				i++;
+			}*/
+			
+			JTable dataTable = new JTable(data, columns);
+			dataTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+/*			Enumeration<TableColumn> modelEnum = dataTable.getColumnModel().getColumns();
+				
+				while (modelEnum.hasMoreElements()) {
+					((TableColumn) modelEnum.nextElement()).setCellRenderer(new DefaultTableCellRenderer() {
+
+						private static final long serialVersionUID = 1L;
+
+						@Override
+						public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+								boolean hasFocus, int row, int column) {
+							Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row,
+									column);
+							if (column == 0 || row > 3) {
+								c.setBackground(Color.LIGHT_GRAY);
+							}
+							return c;
+						}
+					});
+				}*/
+
+/*			dataTable.getColumnModel().getColumn(0).setCellRenderer(new DefaultTableCellRenderer() {
+		        Color backgroundColor = getBackground();
+
+		        @Override
+		        public Component getTableCellRendererComponent(
+		            JTable table, Object value, boolean isSelected,
+		            boolean hasFocus, int row, int column) {
+		            Component c = super.getTableCellRendererComponent(
+		                table, value, isSelected, hasFocus, row, column);
+		            c.setBackground(Color.LIGHT_GRAY);
+		            return c;
+		        }
+			});*/
+
+//			((DefaultTableCellRenderer)dataTable.getColumnModel().getColumn(0).()).setBackground(Color.LIGHT_GRAY);
+//			dataTable.getColumnModel().getColumn(0).setCellRenderer(new DefaultTableCellHeaderRenderer());
+					
+	        setContentPane(new JScrollPane(dataTable, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED)); 
+
+		} catch (BulkDataException e) {
+			e.printStackTrace();
+		}
+		
+//		ColumnDataPopupPanel popup =
+//			new ColumnDataPopupPanel(value, colDef, isModelEditable);
+//		popup.setUserActionInfo(this, row, col, table);
+//		setContentPane(popup);
+		
+		GUIUtils.enableCloseByEscape(this);
+	}
+	
+	   
    }
 
 }
